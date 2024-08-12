3ce57915e69aa89ebe82edd6514a5b2a0b3f4b73
Fahad Hasan <44fahadhasan@gmail.com>
2024-08-12 19:39:27 +0600

    cmnt code

--- src/middleware.js
+++ src/middleware.js
@@ -4,11 +4,12 @@ import { NextResponse } from "next/server";
 export const middleware = (req) => {
   const token = cookies(req).get("next-auth.session-token");
 
-  const pathName = req.nextUrl.pathname;
+  // const pathName = req.nextUrl.pathname;
 
   if (!token) {
     return NextResponse.redirect(
-      new URL(`/login?redirectKoro=${pathName}`, req.url)
+      // new URL(`/login?redirectKoro=${pathName}`, req.url)
+      new URL("/login", req.url)
     );
   }
 
